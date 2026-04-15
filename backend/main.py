71ee5f291b953cd02ecda3752f1af763f8e2ff0a
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-04-15 21:11:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -72,9 +72,3 @@ def expand(req: RequestBody):
         "keyword": word,
         "keywords": list(set(results))[:8] # Returns top 8 unique keywords
     }
-
-if __name__ == "__main__":
-    # Render provides a $PORT environment variable
-    port = int(os.environ.get("PORT", 8000)) 
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port)
