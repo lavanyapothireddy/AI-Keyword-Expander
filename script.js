3e3b58ccf43a892ff8bba13a229ff24b878084a2
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-04-15 14:26:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+async function expand() {
+    let keyword = document.getElementById("keyword").value;
+
+    if (!keyword) {
+        alert("Please enter a keyword");
+        return;
+    }
+
+    let list = document.getElementById("result");
+    list.innerHTML = "<li>⏳ Processing...</li>";
+
+    try {
+        let res = await fetch("http://127.0.0.1:8000/expand", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ keyword: keyword })
+        });
+
+        let data = await res.json();
+
+        list.innerHTML = "";
+
+        if (data.keywords.length === 0) {
+            list.innerHTML = "<li>No keywords found</li>";
+            return;
+        }
+
+        data.keywords.forEach(k => {
+            let li = document.createElement("li");
+            li.innerText = k;
+            list.appendChild(li);
+        });
+
+    } catch (error) {
+        list.innerHTML = "<li>❌ Error connecting to server</li>";
+    }
+}
