71edea71f26af78d37b2ab2fc6075d2a0844a669
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-04-15 20:42:57 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -74,5 +74,7 @@ def expand(req: RequestBody):
     }
 
 if __name__ == "__main__":
+    # Render provides a $PORT environment variable
+    port = int(os.environ.get("PORT", 8000)) 
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
