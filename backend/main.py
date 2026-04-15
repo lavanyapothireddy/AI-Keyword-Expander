b646febfef964ded29e2f1af813603727796ce72
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2026-04-15 16:10:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -66,5 +66,12 @@ def expand(data: InputData):
 # Render support
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    import os
+
+    port = int(os.environ.get("PORT", 10000))
+
+    uvicorn.run(
+        "backend.main:app",
+        host="0.0.0.0",
+        port=port
+    )
